327ad5ac28116ed1281b476b69c20a359c27a6f2
VooDooBlooD <mdan.hagh@gmail.com>
2015-11-02 00:17:23 +0330

    Replace sessions with access.py funcs

--- project/apps/user/controller.py
+++ project/apps/user/controller.py
@@ -3,11 +3,12 @@ __author__ = 'AminHP'
 
 
 #flask import
-from flask import request, session, render_template
+from flask import request, render_template
 
 #project import
 from project.apps.user import user
 from project.apps.user.forms import Login
+from project.utils.access import login_user, logout_user
 
 
 @user.route('login/', methods=['GET', 'POST'])
@@ -15,12 +16,12 @@ def login():
 	form = Login()
 	if request.method == 'POST':
 		if form.validate_on_submit():
-			session['username'] = form.username
+			login_user(form.username)
 			return "success"
 	return render_template('login.html', form=form)
 
 
 @user.route('logout/', methods=['GET'])
 def logout():
-	session.pop('username')
+	logout_user()
 	return render_template('logout.html')
