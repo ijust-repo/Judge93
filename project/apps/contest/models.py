ebb2d82989c1e0014d3d7330928af8d24d06a9ac
VooDooBlooD <mdan.hagh@gmail.com>
2015-11-02 21:44:33 +0330

    Add Problems and Teams to Contest collection

--- project/apps/contest/models.py
+++ project/apps/contest/models.py
@@ -2,17 +2,42 @@
 __author__ = 'AminHP'
 
 # python imports
-from mongoengine import Document, StringField, IntField, DateTimeField, ReferenceField, ListField, EmbeddedDocument, EmbeddedDocumentField
+from mongoengine import Document, StringField, IntField, BooleanField, DateTimeField, ReferenceField, ListField, EmbeddedDocument, EmbeddedDocumentField
+
+
+class Testcase(EmbeddedDocument):
+	order = IntField(required=True, unique=True)
+	input = StringField(required=True)
+	output = StringField(required=True)
+
+
+class Problem(EmbeddedDocument):
+	order = IntField(required=True, unique=True)
+	title = StringField(required=True)
+	header = StringField()
+	body = StringField(required=True)
+	testcases = ListField(EmbeddedDocumentField(Testcase))
+	footer = StringField()
+
+
+
+class Result(EmbeddedDocument):
+	problem = ReferenceField('Problem', required=True, unique=True)
+	status = StringField()
+	penalty = IntField()
+	solved = BooleanField()
 
 
 class TeamInfo(EmbeddedDocument):
-	team = ReferenceField('Team', required=True)
-	penalty = IntField()
+	team = ReferenceField('Team', required=True, unique=True)
+	problem_results = ListField(EmbeddedDocumentField(Result))
+
 
 
 class Contest(Document):
 	owner = ReferenceField('User', required=True)
 	name = StringField(required=True)
-	start_time = DateTimeField(required=True)
+	starts_on = DateTimeField(required=True)
 	duration = IntField(required=True)
 	teams = ListField(EmbeddedDocumentField(TeamInfo))
+	problems = ListField(EmbeddedDocumentField(Problem))
