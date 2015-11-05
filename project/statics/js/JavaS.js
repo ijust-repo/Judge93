44184e288edf0732ab6dd9c4bee446ebc10e94f3
talayeh_R <talayeh.riahi@gmail.com>
2015-11-05 13:05:40 +0330

    signuppage ajax request completed

--- project/statics/js/JavaS.js
+++ project/statics/js/JavaS.js
@@ -13,19 +13,16 @@ var password = $("input#password").val();
             dataType: "json",
             data: '{"username": "' + username + '", "password" : "' + password + '"}',
             success: function (data) {
-              $("#result").text(data.username + data.password);
-    			
-
+              $("#result").text(data.username + data.password);    	
             },
             error: function (request, status, error) {
-            	
+      	
               alert( request.status);
-             
-              
+
             }
 		});
 	});
 });
 /*****************************************slideshow*********************************/
 
-/*****************************************slideshow*********************************/
+/*************************************slideshow*********************************/
