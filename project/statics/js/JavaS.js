4cbef57e17254c49c7ff148289cb4e470c4427df
talayeh_R <talayeh.riahi@gmail.com>
2015-11-07 09:36:44 +0330

    home.html edited

--- project/statics/js/JavaS.js
+++ project/statics/js/JavaS.js
@@ -13,11 +13,14 @@ var password = $("input#password").val();
             dataType: "json",
             data: '{"username": "' + username + '", "password" : "' + password + '"}',
             success: function (data) {
-              $("#result").text(data.username + data.password);    	
+              	
             },
             error: function (request, status, error) {
-      	
-              alert( request.status );
+      	if (request.status===201) {
+      		alert( request.status );
+      		window.location.replace("{{ url_for('/') + 'user/home/'}}");
+      		}
+              
 
             }
 		});
