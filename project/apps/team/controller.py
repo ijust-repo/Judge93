203d2d157939bb02f3df42f622f5dbed636691ad
Salar <salarnasiri74@gmail.com>
2015-12-29 12:45:33 +0330

    team can not join to contests if it has been finished

--- project/apps/team/controller.py
+++ project/apps/team/controller.py
@@ -211,6 +211,10 @@ def join_request():
 			team_obj = Team.objects().get(name=team_name)
 			if logged_in_user() != team_obj.owner.username:
 				return jsonify(errors = "user is not team owner") , 403
+				
+			if contest_obj.ends_on()<datetime.utcnow():
+				return jsonify(errors = "this contest has been finished") , 403
+
 			team_members_with_owner = copy(team_obj.members)
 			team_members_with_owner.append(team_obj.owner)
 
