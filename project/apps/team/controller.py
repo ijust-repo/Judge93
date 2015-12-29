922c6867de9daaffd2776c89ef0dff87f4c80639
VooDooBlooD <mdan.hagh@gmail.com>
2015-12-30 02:54:36 +0330

    Change error types for add_member

--- project/apps/team/controller.py
+++ project/apps/team/controller.py
@@ -111,11 +111,9 @@ def add_member():
 			try:
 				user_obj = User.objects.get(username=member)
 			except DoesNotExist:
-				form.members.errors.append(form.members.gettext('User does not exist!'))
-				return jsonify(errors=form.errors), 406
+				return jsonify(errors='User does not exist!'), 406
 			if user_obj in team_obj.members:
-				form.members.errors.append(form.members.gettext('No one can be added twice!'))
-				return jsonify(errors=form.errors), 406
+				return jsonify(errors='No one can be added twice!'), 406
 			else:
 				team_obj.members.append(user_obj)
 				user_obj.teams.append(team_obj)
